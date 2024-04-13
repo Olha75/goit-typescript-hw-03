4794a4e3f1b3e5dbd0e37bedf09d1f0b87ccf9bf
Olha75 <137830839+Olha75@users.noreply.github.com>
2024-04-13 12:54:42 +0300

    task_4

--- src/4.ts
+++ src/4.ts
@@ -1,8 +1,49 @@
+
+class Key {
+    private signature: number;
+
+    // constructor() { this.signature = Math.random(); }
+
+  public getSignature():number {
+    return this.signature;
+  }
+}
+
+class Person  {
+    constructor( private key: Key){}
+    private getKey(){
+      return this.key;
+    }
+}
+
+abstract class House{
+protected door:boolean=false;
+protected tenants: Person []=[];
+comeIn(tenant:Person){
+  if(this.door===true){this.tenants.push(tenant)}
+}
+abstract OpenDoor(key:Key):void;
+constructor(protected key:Key){}
+}
+
+class MyHouse extends House{
+  openDoor(key:Key):void{
+    if (this.key.getSignature()===key.getSignature()){
+      this.door=true;
+    }
+  }
+}
+
+
+
+
+
+
+
 const key = new Key();
 
 const house = new MyHouse(key);
 const person = new Person(key);
-
 house.openDoor(person.getKey());
 
 house.comeIn(person);
